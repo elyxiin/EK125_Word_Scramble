8c84a557b6f280f673bf8de8e5ca2337a2cde1a6
Xin Ngo <xinyngo@gmail.com>
2026-03-26 17:01:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,111 +1,18 @@
 # imports
-import csv
-import random
 import game_logic 
-# reading data from 'word_bank'
-with open('word_bank.csv', mode='r', newline='') as file:
-    reader = csv.DictReader(file)
-
-    # categories
-    categories = {
-        'animal' : [],
-        'food' : [],
-        'country' : [],
-        'science' : [],
-        'technology' : [] 
-        }
-
-    for row in reader:
-        for key in categories:
-            if row[key]: # since some categories have more values than others, checks to see if it is empty
-                categories[key].append(row[key])
-
-def selectCategory():
-    """Allows user to select category either by typing in the category or corresponding number"""
-    category = 0
-
-    # category menu
-    user = input("Please select a category\n\
-                 1. Animals \n\
-                 2. Foods \n\
-                 3. Countries \n\
-                 4. Science \n\
-                 5. Technology\n").lower()
-    
-    if user=='animals' or user=='1':
-        category = 'animal'
-    elif user=='foods' or user=='2':
-        category = 'food'
-    elif user=='countries' or user=='3':
-        category = 'country'
-    elif user=='science' or user=='4':
-        category = 'science'
-    elif user=='technology' or user=='5':
-        category = 'technology'
-    else:
-        print(f"{user} is not a valid category.")
-        selectCategory()
-    return category
-    
-def selectDifficulty(category):
-    """Returns player selected difficulty and a refined word bank only including words of the selected difficulty within the selected category."""
-    difficulty = ''
-    wordBank = None
-    user = input("Please select a difficulty: \n" \
-    "1. Easy \n" \
-    "2. Medium \n" \
-    "3. Hard \n").lower()
-
-    if user == 'easy' or user=='1':
-        difficulty='easy'
-        wordBank = [word for word in categories[category] if len(word)<6]
-    elif user == 'medium' or user=='2':
-        difficulty='medium'
-        wordBank = [word for word in categories[category] if 5<len(word) and len(word)<9]
-    elif user == 'hard' or user=='3':
-        difficulty='hard'
-        wordBank = [word for word in categories[category] if 8<len(word)]
-    else:
-        print(f"\n{user} is not a valid difficulty.")
-        selectDifficulty(category)
-    
-    return difficulty, wordBank
-
-def scramble(word):
-    lstWord = list(word)
-    chars = []
-
-    # Works by selecting a random index and removing that letter from the pool until there's no letters left in the original and they have all been added to the new
-    for i in range(len(word)):
-        rand = random.randint(0,len(lstWord)-1) # random index integer
-        chars.append(lstWord.pop(rand))
-    scrambled = "".join(chars) # joins the list into a string
-    
-    # check to make sure the word is scrambled
-    if scrambled == word:
-        scramble(word)
-
-    return scrambled
-
-def selectWord(wordBank):
-    """Selects a scrambled word from the word bank."""
-    rand = random.randint(0,len(wordBank)-1)
-    word = wordBank[rand]
-    scrambled = scramble(word)
-    return word, scrambled
-
-
+import scramble
 
 print("Welcome to Guess the Scrambled Word!")
 isOn = True
 
 while(True):
-    category = selectCategory()
-    difficulty, wordBank = selectDifficulty(category)
-    word, scrambled = selectWord(wordBank)
+    category = scramble.selectCategory()
+    difficulty, wordBank = scramble.selectDifficulty(category)
+    word, scrambled = scramble.selectWord(wordBank)
     print(f"The scrambled word is {scrambled}")
 
     score = game_logic.playGame(scramble, word, difficulty)
 
     input("Press enter to play again. ")
 
+m
