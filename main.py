54e24a7a9ad1128f0e7d630ada070cb86cf0f6c7
kierar-creator <kierar@bu.edu>
2026-03-26 14:24:04 -0400

    part c

--- main.py
+++ main.py
@@ -108,3 +108,42 @@ print(f"\nFull word bank: {categories[category]}")
 print(f"\nRefined word bank: {wordBank}")
 print(f"\nSelected word: {word}")
 print(f"Scrambled word: {scrambled}")
+
+#Part C
+
+# hints
+def selectHint():
+    hint = 0
+    hint_select = input("Please select a hint\n\
+        1. First Letter\n\
+        2. Last Letter\n\
+        3. None (Guess the word)\n")
+
+    if hint_select=='' or hint_select =='1':
+        hint = 'First Letter'
+    elif hint_select =='' or hint_select =='2':
+        hint = ('Last Letter')
+
+    elif hint_select == 'None' or hint_select =='3':
+        hint = 'None'
+    else:
+        print(f"{hint_select} is not a valid Hint.")
+        selectHint()
+    return hint
+
+
+score = 1000
+#def show_word_length(word):
+   # """reveal the length of the word"""
+    #length = len(word)
+    #print("The word has", length, "letters.")
+
+
+hint = selectHint()
+print("Hint chosen:", hint)
+if hint == "First Letter":
+    score = score-2
+    print("First letter:", word[0])
+elif hint == "Last Letter":
+    score = score-2
+    print("Last letter:", word[-1])
